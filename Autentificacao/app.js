cc58c41bae76ac9d61be171211f8fdc0d13d1bd7
Falape <filipe_s_fernandes@hotmail.com>
2022-06-06 13:13:57 +0100

    servidor para autentificação a utiliza passport e a devolver token jwt

---
+++ Autentificacao/app.js
@@ -0,0 +1,106 @@
+var express = require("express"),
+  mongoose = require("mongoose"),
+  bodyParser = require("body-parser"),
+  User = require("./models/user"),
+  passport = require("passport"),
+  LocalStrategy = require("passport-local"),
+  passportLocalMongoose = require("passport-local-mongoose"),
+  jwt = require('jsonwebtoken');
+var logger = require('morgan');
+
+
+mongoose.connect("mongodb://localhost:27017/RRD", { useNewUrlParser: true });
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ type: 'application/json' }));
+app.use(require("express-session")({
+  secret: "O Ramalho e fixe",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(logger('dev'));
+
+// handeling user sign up
+app.post("/registo", function (req, res) {
+  console.log(req.body);
+  //console.log(req.body.password);
+  User.register(new User({ username: req.body.username, email: req.body.email, level: 'consumer'}), req.body.password, function (err, user) {
+    if (err) {
+      console.log(err);
+      return res.status(400).jsonp({
+        message: 'User já existe!', user: user
+      });
+    }
+    passport.authenticate('local', { session: false }, (err, user, info) => {
+      console.log(err)
+      console.log(user)
+      if (err || !user) {
+        return res.status(500).jsonp({error:"Erro na Autentificação", err:err})
+      }
+      req.login(user, { session: false }, (err) => {
+        if (err) {
+          return res.status(500).jsonp({error:"Erro no login"})
+        }
+        // generate a signed son web token with the contents of user object and return it in the response
+        jwt.sign({ _id:user._id}, 'O Ramalho e fixe',
+                {expiresIn: '10m'}, 
+                function(e, token){
+                  if(e) res.status(507).jsonp({error:"Erro na geração de token"})
+                  else res.status(201).jsonp({token:token})
+        });
+      });
+    })(req, res);
+  });
+});
+
+// Login Logic
+// middleware
+app.post("/login", function (req, res) {
+
+  passport.authenticate('local', { session: false }, (err, user, info) => {
+    if (err || !user) {
+      return res.status(400).json({
+        message: 'Something is not right', user: user
+      });
+    }
+    req.login(user, { session: false }, (err) => {
+      if (err) {
+        return res.status(500).jsonp({error:"Erro no login"});
+      }
+      // generate a signed son web token with the contents of user object and return it in the response
+      jwt.sign({ _id:user._id}, 'O Ramalho e fixe',
+                {expiresIn: '10m'}, 
+                function(e, token){
+                  if(e) res.status(507).jsonp({error:"Erro na geração de token"})
+                  else res.status(201).jsonp({token:token})
+        });
+    });
+  })(req, res);
+
+});
+
+// Logout
+app.get("/logout", function (req, res) {
+  req.logout();
+});
+
+// check isLoggedIn
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
+}
+
+
+app.listen(7001, process.env.IP, function () {
+  console.log("Servidor à escuta na porta 7001...")
+});
