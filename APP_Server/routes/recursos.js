cad8c64d5c014361a9d7e521c51387b0e3eea4ea
inesmarinho <inesmarinho1404@gmail.com>
2022-06-18 17:47:29 +0100

    pagina de novos recursos

--- APP_Server/routes/recursos.js
+++ APP_Server/routes/recursos.js
@@ -2,6 +2,27 @@ var express = require('express');
 var router = express.Router();
 var axios = require('axios')
 
+// get página de novo recurso
+router.get('/new/new', function(req, res) {
+    console.log("consegui chegar aqui")
+    res.render('novo_recurso');
+});
+
+// post do novo recurso
+// router.post('/new/new', function(req, res) {
+//     req.body.user = req.cookies.data.userData.username
+//     console.log("req.body " + JSON.stringify(req.body))
+//     // pedido a server
+//     axios.post('http://localhost:7002/recursos?token=' + req.cookies.data.token, req.body)
+//         .then(dados => {
+//             console.log("TESTE")
+//             res.redirect('/inicio')
+//         })
+//         .catch(e => {
+//             console.log(e)
+//             res.render('error', {error: e})
+//         })
+// });
 
 router.get('/:id', function (req, res) {
     console.log("entra")
@@ -15,3 +36,10 @@ router.get('/:id', function (req, res) {
         })
         .catch(e => res.render('error', { error: e }))
 });
+
+module.exports = router;
+
+
+
+
+
